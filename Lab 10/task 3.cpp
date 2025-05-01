3143eef69727fbc9e1070f20062fe9cabe9215f9
Muhammad Hammad <hammadamin73@gmail.com>
2025-05-01 20:02:05 +0500

    Create task 3.cpp

---
+++ Lab 10/task 3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream file;
+    string line;
+    string type;
+    string ID;
+    string name;
+    string year;
+    int Year;
+    string extra;
+    string certification;
+    file.open("vehicles.txt");
+
+    if(!file.is_open())
+    {
+        cout << "Error file vehicles.txt cannot be opened" << endl;
+    }
+    else
+    {
+        while(getline(file , line))
+        {
+            if(!line.empty() && line[0] != '/')
+            {
+                
+                istringstream rd(line);
+                getline(rd,type,',');
+                getline(rd,ID,',');
+                getline(rd,name,',');
+                getline(rd,year,',');
+                getline(rd,extra,',');
+                getline(rd,certification,',');
+                cout << "Type: "<< type <<", ID: " << ID << ", Year: " << year << ", ExtraData: " << extra << ", Certification: " << certification << endl;
+                Year = stoi(year);
+                cout << "Integer Year for: " << type << " is " << Year << endl;
+            }
+        }
+    }
+    file.close();
+}
